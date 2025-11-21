c4f5b2f00c511bb396333cdd3be2cd90b4478c28
epepei <manya.ermakova@gmail.com>
2025-11-21 23:31:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define VECTOR_TYPE int
-
 #include "number2.c"
-
 int main() {
-    
     test_create_and_basic();
     test_push_back_capacity();
     test_delete_at();
     test_is_equal();
     test_copy_operations();
     test_edge_cases();
-    
     printf("ALL TESTS PASSED! Vector library works correctly!\n");
-    
     return 0;
 }
